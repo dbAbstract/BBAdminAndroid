c41e3342b8670a180adb3f3e6b0f7ee7ede52b21
taki.enari <taqi.enari@rakuten.com>
2023-12-16 21:24:20 +0200

    Create LocalDateTimeUtil.kt

---
+++ core/src/main/java/za/co/bb/core/util/LocalDateTimeUtil.kt
@@ -0,0 +1,17 @@
+package za.co.bb.core.util
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
+
+val now: kotlinx.datetime.LocalDateTime
+    get() = Clock.System.now().toLocalDateTime(timeZone = TimeZone.currentSystemDefault())
+
+fun fromEpochSeconds(epochSeconds: Long): LocalDateTime =
+    Instant.fromEpochSeconds(epochSeconds).toLocalDateTime(TimeZone.currentSystemDefault())
+
+fun toEpochSeconds(localDateTime: LocalDateTime): Long =
+    localDateTime.toInstant(TimeZone.currentSystemDefault()).epochSeconds
