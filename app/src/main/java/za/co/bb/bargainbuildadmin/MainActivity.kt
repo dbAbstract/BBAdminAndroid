5e4bc83a98110828ef61dee55a9bc878aec2e96d
taki.enari <taqi.enari@rakuten.com>
2023-12-16 18:09:51 +0200

    Main screen changes

--- app/src/main/java/za/co/bb/bargainbuildadmin/MainActivity.kt
+++ app/src/main/java/za/co/bb/bargainbuildadmin/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -39,10 +45,24 @@ class MainActivity : ComponentActivity() {
                         .align(Alignment.BottomCenter)
                         .fillMaxWidth()
                         .height(80.dp)
-                        .navigationBarsPadding()
                         .background(AppColors.current.primary)
                 ) {
-
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .navigationBarsPadding(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        IconButton(
+                            onClick = {}
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Person,
+                                contentDescription = null
+                            )
+                        }
+                    }
                 }
             }
         }
