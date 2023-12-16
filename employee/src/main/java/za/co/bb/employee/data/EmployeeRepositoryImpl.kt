acb681764c89cc1370b17ad9fe0a2bcc9baa7e24
taki.enari <taqi.enari@rakuten.com>
2023-12-16 18:02:55 +0200

    Fix bug in EmployeeRepositoryImpl

--- employee/src/main/java/za/co/bb/employee/data/EmployeeRepositoryImpl.kt
+++ employee/src/main/java/za/co/bb/employee/data/EmployeeRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package za.co.bb.employee.data
 
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import za.co.bb.employee.domain.EmployeeRepository
 import za.co.bb.employee.domain.model.Employee
 import kotlin.coroutines.resume
@@ -10,23 +12,27 @@ internal class EmployeeRepositoryImpl(
     private val firebaseFirestore: FirebaseFirestore
 ) : EmployeeRepository {
 
-    override suspend fun getEmployees(): Result<List<Employee>> = suspendCoroutine { continuation ->
-        firebaseFirestore.collection(EMPLOYEE_TABLE).get()
-            .addOnSuccessListener { result ->
-                continuation.resume(
-                    Result.success(
-                        result.documents.mapNotNull { it.toObject(Employee::class.java) }
+    override suspend fun getEmployees(): Result<List<Employee>> = withContext(Dispatchers.IO) {
+        suspendCoroutine { continuation ->
+            firebaseFirestore.collection(EMPLOYEE_TABLE).get()
+                .addOnSuccessListener { result ->
+                    continuation.resume(
+                        Result.success(
+                            result.toObjects(EmployeeEntity::class.java).mapNotNull {
+                                it.toEmployee()
+                            }
+                        )
                     )
-                )
-            }
-            .addOnCanceledListener {
-                continuation.resume(
-                    Result.failure(Throwable(message = "Retrieval of employees cancelled."))
-                )
-            }
-            .addOnFailureListener {
-                continuation.resume(Result.failure(it))
-            }
+                }
+                .addOnCanceledListener {
+                    continuation.resume(
+                        Result.failure(Throwable(message = "Retrieval of employees cancelled."))
+                    )
+                }
+                .addOnFailureListener {
+                    continuation.resume(Result.failure(it))
+                }
+        }
     }
 
     companion object {
--- employee/src/main/java/za/co/bb/employee/domain/model/Employee.kt
+++ employee/src/main/java/za/co/bb/employee/domain/model/Employee.kt
@@ -1,18 +1,9 @@
 package za.co.bb.employee.domain.model
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class Employee(
-    @SerialName("id")
     val id: String,
-    @SerialName("firstName")
     val firstName: String,
-    @SerialName("surname")
     val surname: String,
-    @SerialName("middleName")
     val middleName: String? = null,
-    @SerialName("age")
     val age: Int
 )
