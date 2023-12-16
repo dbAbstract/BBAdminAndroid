383696349eda0589e434419fbca50b2276a42a5a
taki.enari <taqi.enari@rakuten.com>
2023-12-16 22:16:54 +0200

    Optimize GetWageStatusForEmployees

--- home-screen/src/main/java/za/co/bb/home/domain/usecase/GetWageStatusForEmployees.kt
+++ home-screen/src/main/java/za/co/bb/home/domain/usecase/GetWageStatusForEmployees.kt
@@ -1,6 +1,8 @@
 package za.co.bb.home.domain.usecase
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import za.co.bb.employees.domain.model.Employee
 import za.co.bb.employees.domain.repository.EmployeeRepository
@@ -14,27 +16,37 @@ internal class GetWageStatusForEmployees(
     private val workHoursRepository: WorkHoursRepository
 ) {
     suspend fun execute(): List<WageStatus> = withContext(Dispatchers.IO) {
-        var wageStatuses: List<WageStatus> = emptyList()
-        ifSuccessfullyRetrievedEmployees { employees ->
-            wageStatuses = employees.mapNotNull { employee ->
-                val wageResult = wageRepository.getCurrentWageForEmployee(employeeId = employee.id)
-                val workHoursDueResult = workHoursRepository.getHoursDue(employeeId = employee.id)
-                if (wageResult.isSuccess && workHoursDueResult.isSuccess) {
-                    val wage = wageResult.getOrThrow()
-                    val workHours = workHoursDueResult.getOrThrow()
+        coroutineScope {
+            var wageStatuses: List<WageStatus> = emptyList()
+            ifSuccessfullyRetrievedEmployees { employees ->
+                wageStatuses = employees.mapNotNull { employee ->
+                    val wageResultDeferred = async {
+                        wageRepository.getCurrentWageForEmployee(employeeId = employee.id)
+                    }
+                    val workHoursDueResultDeferred = async {
+                        workHoursRepository.getHoursDue(employeeId = employee.id)
+                    }
 
-                    WageStatus(
-                        employee = employee,
-                        wage = wageResult.getOrThrow(),
-                        amountDue = wage.amount * workHours.hours,
-                        hoursUnpaid = workHours.hours
-                    )
-                } else {
-                    null
+                    val wageResult = wageResultDeferred.await()
+                    val workHoursDueResult = workHoursDueResultDeferred.await()
+
+                    if (wageResult.isSuccess && workHoursDueResult.isSuccess) {
+                        val wage = wageResult.getOrThrow()
+                        val workHours = workHoursDueResult.getOrThrow()
+
+                        WageStatus(
+                            employee = employee,
+                            wage = wageResult.getOrThrow(),
+                            amountDue = wage.amount * workHours.hours,
+                            hoursUnpaid = workHours.hours
+                        )
+                    } else {
+                        null
+                    }
                 }
             }
+            wageStatuses
         }
-        wageStatuses
     }
 
     private suspend fun ifSuccessfullyRetrievedEmployees(
