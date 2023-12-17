50743b7c4f17f2161c30e7ff96d675782daeda68
taki.enari <taqi.enari@rakuten.com>
2023-12-17 13:17:09 +0200

    Add Logs to WorkHoursRepositoryImpl

--- work-hours/src/main/java/za/co/bb/work_hours/data/WorkHoursRepositoryImpl.kt
+++ work-hours/src/main/java/za/co/bb/work_hours/data/WorkHoursRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package za.co.bb.work_hours.data
 
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.Dispatchers
@@ -15,6 +16,7 @@ internal class WorkHoursRepositoryImpl(
 ) : WorkHoursRepository {
     override suspend fun getHoursDueForEmployee(employeeId: String): Result<List<WorkHours>> = withContext(Dispatchers.IO) {
         suspendCoroutine { continuation ->
+            Log.i(TAG, "Getting work hours for $employeeId")
             firebaseFirestore.collection(WORK_HOURS_TABLE)
                 .whereEqualTo(COLUMN_EMPLOYEE_ID, employeeId)
                 .get()
@@ -22,6 +24,7 @@ internal class WorkHoursRepositoryImpl(
                     try {
                         if (result.isEmpty) {
                             continuation.resume(Result.failure(NoWorkHoursFoundException()))
+                            Log.i(TAG, "Empty result set for empId=$employeeId")
                         }
 
                         val workHourSet = mutableSetOf<WorkHours>()
@@ -30,20 +33,24 @@ internal class WorkHoursRepositoryImpl(
                             val workHourEntity = document.toObject<WorkHoursEntity>()
                             val workHours = workHourEntity.toWorkHours(workHoursId = workHourId)
                             workHours?.let {
+                                Log.i(TAG, "Found WorkHours $it")
                                 workHourSet.add(it)
                             }
                         }
                         continuation.resume(Result.success(workHourSet.toList()))
                     } catch (t: Throwable) {
+                        Log.e(TAG, "Error getting WorkHours for $employeeId | error=$t")
                         continuation.resume(Result.failure(t))
                     }
                 }
                 .addOnCanceledListener {
+                    Log.d("lol", "Error getting work hours for $employeeId - Cancelled")
                     continuation.resume(
                         Result.failure(Throwable(message = "Retrieval of work hours cancelled."))
                     )
                 }
                 .addOnFailureListener {
+                    Log.d("lol", "Error getting work hours for $employeeId - Failed")
                     continuation.resume(Result.failure(it))
                 }
         }
@@ -52,5 +59,6 @@ internal class WorkHoursRepositoryImpl(
     companion object {
         private const val WORK_HOURS_TABLE = "work-hours"
         private const val COLUMN_EMPLOYEE_ID = "employeeId"
+        private const val TAG = "WorkHoursRepo"
     }
 }
