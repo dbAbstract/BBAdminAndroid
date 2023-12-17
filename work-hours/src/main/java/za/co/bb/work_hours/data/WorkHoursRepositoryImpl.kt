c8c44ca7afbf08954bb1553e4bfc2817be7a2ca4
taki.enari <taqi.enari@rakuten.com>
2023-12-17 18:54:29 +0200

    Add caching strategy to WorkHoursRepository

--- work-hours/src/main/java/za/co/bb/work_hours/data/WorkHoursRepositoryImpl.kt
+++ work-hours/src/main/java/za/co/bb/work_hours/data/WorkHoursRepositoryImpl.kt
@@ -2,9 +2,13 @@ package za.co.bb.work_hours.data
 
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlinx.datetime.LocalDateTime
+import za.co.bb.core.util.now
+import za.co.bb.core.util.toEpochSeconds
 import za.co.bb.work_hours.domain.NoWorkHoursFoundException
 import za.co.bb.work_hours.domain.WorkHours
 import za.co.bb.work_hours.domain.WorkHoursRepository
@@ -14,51 +18,72 @@ import kotlin.coroutines.suspendCoroutine
 internal class WorkHoursRepositoryImpl(
     private val firebaseFirestore: FirebaseFirestore
 ) : WorkHoursRepository {
-    override suspend fun getHoursDueForEmployee(employeeId: String): Result<List<WorkHours>> = withContext(Dispatchers.IO) {
-        suspendCoroutine { continuation ->
-            Log.i(TAG, "Getting work hours for $employeeId")
-            firebaseFirestore.collection(WORK_HOURS_TABLE)
-                .whereEqualTo(COLUMN_EMPLOYEE_ID, employeeId)
-                .get()
-                .addOnSuccessListener { result ->
-                    try {
-                        if (result.isEmpty) {
-                            continuation.resume(Result.failure(NoWorkHoursFoundException()))
-                            Log.i(TAG, "Empty result set for empId=$employeeId")
-                        }
+    private val workHistoryCache = mutableMapOf<EmployeeId, List<WorkHours>>()
+    private var lastRefreshed: LocalDateTime? = null
+    private val cacheTime: Long = 60 * 5 // 5 Minutes
 
-                        val workHourSet = mutableSetOf<WorkHours>()
-                        for (document in result) {
-                            val workHourId = document.id
-                            val workHourEntity = document.toObject<WorkHoursEntity>()
-                            val workHours = workHourEntity.toWorkHours(workHoursId = workHourId)
-                            workHours?.let {
-                                Log.i(TAG, "Found WorkHours $it")
-                                workHourSet.add(it)
-                            }
-                        }
-                        continuation.resume(Result.success(workHourSet.toList()))
-                    } catch (t: Throwable) {
-                        Log.e(TAG, "Error getting WorkHours for $employeeId | error=$t")
-                        continuation.resume(Result.failure(t))
-                    }
-                }
-                .addOnCanceledListener {
-                    Log.d("lol", "Error getting work hours for $employeeId - Cancelled")
-                    continuation.resume(
-                        Result.failure(Throwable(message = "Retrieval of work hours cancelled."))
-                    )
-                }
-                .addOnFailureListener {
-                    Log.d("lol", "Error getting work hours for $employeeId - Failed")
-                    continuation.resume(Result.failure(it))
-                }
+    override suspend fun getHoursDueForEmployee(employeeId: String): Result<List<WorkHours>> = withContext(Dispatchers.IO) {
+        val isCacheValid = lastRefreshed?.let {
+            (toEpochSeconds(now) - toEpochSeconds(it) < cacheTime)
+                    && workHistoryCache.isNotEmpty()
+                    && workHistoryCache[employeeId]?.isNotEmpty() == true
+        } ?: false
+
+        return@withContext when {
+            isCacheValid -> Result.success(workHistoryCache[employeeId]!!)
+
+            else -> getWorkHours(employeeId)
         }
     }
+
+    private suspend fun getWorkHours(employeeId: EmployeeId): Result<List<WorkHours>> = suspendCoroutine { continuation ->
+        Log.i(TAG, "Getting work hours for $employeeId")
+        firebaseFirestore.collection(WORK_HOURS_TABLE)
+            .whereEqualTo(COLUMN_EMPLOYEE_ID, employeeId)
+            .orderBy(COLUMN_CREATION_DATE, Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                try {
+                    if (result.isEmpty) {
+                        continuation.resume(Result.failure(NoWorkHoursFoundException()))
+                        Log.i(TAG, "Empty result set for empId=$employeeId")
+                    }
+
+                    val workHourSet = mutableSetOf<WorkHours>()
+                    for (document in result) {
+                        val workHourId = document.id
+                        val workHourEntity = document.toObject<WorkHoursEntity>()
+                        val workHours = workHourEntity.toWorkHours(workHoursId = workHourId)
+                        workHours?.let {
+                            Log.i(TAG, "Found WorkHours $it")
+                            workHourSet.add(it)
+                        }
+                    }
+                    continuation.resume(Result.success(workHourSet.toList()))
+                } catch (t: Throwable) {
+                    Log.e(TAG, "Error getting WorkHours for $employeeId | error=$t")
+                    continuation.resume(Result.failure(t))
+                }
+            }
+            .addOnCanceledListener {
+                Log.d("lol", "Error getting work hours for $employeeId - Cancelled")
+                continuation.resume(
+                    Result.failure(Throwable(message = "Retrieval of work hours cancelled."))
+                )
+            }
+            .addOnFailureListener {
+                Log.d("lol", "Error getting work hours for $employeeId - Failed")
+                continuation.resume(Result.failure(it))
+            }
+    }
+
 
     companion object {
         private const val WORK_HOURS_TABLE = "work-hours"
         private const val COLUMN_EMPLOYEE_ID = "employeeId"
+        private const val COLUMN_CREATION_DATE = "creationDate"
         private const val TAG = "WorkHoursRepo"
     }
 }
+
+internal typealias EmployeeId = String
