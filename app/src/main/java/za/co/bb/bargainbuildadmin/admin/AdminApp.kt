f8feff960d0858e94e387caddb40d90350d653c9
taki.enari <taqi.enari@rakuten.com>
2023-12-23 13:57:48 +0200

    w.i.p - refactor app structure

--- app/src/main/java/za/co/bb/bargainbuildadmin/admin/AdminApp.kt
+++ app/src/main/java/za/co/bb/bargainbuildadmin/admin/AdminApp.kt
@@ -1,17 +1,20 @@
 package za.co.bb.bargainbuildadmin.admin
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import za.co.bb.app_admin.navigate.adminAppNavGraph
 import za.co.bb.core.navigation.NavAction
+import za.co.bb.core.navigation.Screen
 
 @Composable
 fun AdminApp(navigate: (NavAction) -> Unit) {
     val navController = rememberNavController()
 
-    Box(modifier = Modifier.fillMaxSize()) {
-
+    NavHost(
+        navController = navController,
+        startDestination = Screen.HomeScreen.name
+    ) {
+        adminAppNavGraph(navigate = navigate)
     }
 }
