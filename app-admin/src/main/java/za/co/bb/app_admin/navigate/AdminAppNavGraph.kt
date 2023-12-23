4535eb4788965db7b34f7bd46e601c1a26c060fd
taki.enari <taqi.enari@rakuten.com>
2023-12-23 13:59:29 +0200

    w.i.p - refactor app structure

--- app-admin/src/main/java/za/co/bb/app_admin/navigate/AdminAppNavGraph.kt
+++ app-admin/src/main/java/za/co/bb/app_admin/navigate/AdminAppNavGraph.kt
@@ -2,10 +2,10 @@ package za.co.bb.app_admin.navigate
 
 import androidx.navigation.NavGraphBuilder
 import za.co.bb.core.navigation.NavAction
-import za.co.bb.home.view.homeNavGraph
+import za.co.bb.home.navigation.adminHomeNavGraph
 import za.co.bb.work_status.navigation.workStatusNavGraph
 
 fun NavGraphBuilder.adminAppNavGraph(navigate: (NavAction) -> Unit) {
-    homeNavGraph(navigate)
+    adminHomeNavGraph(navigate)
     workStatusNavGraph(navigate)
 }
